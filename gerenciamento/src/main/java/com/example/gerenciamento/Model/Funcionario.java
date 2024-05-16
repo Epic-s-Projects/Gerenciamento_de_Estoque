255784a1fb96c2276851f7919a4db655085e1306
epicestudar <viniciusftza@gmail.com>
2024-05-16 16:31:50 -0300

    criando as entidades

--- gerenciamento/src/main/java/com/example/gerenciamento/Model/Funcionario.java
+++ gerenciamento/src/main/java/com/example/gerenciamento/Model/Funcionario.java
@@ -2,17 +2,19 @@ package com.example.gerenciamento.Model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Funcionario implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String cpf;
     private String nomeFuncionario;
     private String cargo;
-    
+
     public String getCpf() {
         return cpf;
     }
