74f9bcfd2ab053f1d456aa178e5f3dc9a29b9fa3
epicestudar <viniciusftza@gmail.com>
2024-05-16 14:11:52 -0300

    primeiro commit

---
+++ gerenciamento/src/main/java/com/example/gerenciamento/GerenciamentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoApplication.class, args);
+	}
+
+}
---
+++ gerenciamento/src/test/java/com/example/gerenciamento/GerenciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
