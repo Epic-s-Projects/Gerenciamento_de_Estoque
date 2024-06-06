348b1cf73f54bce13e4228af370f55c60388a5d0
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2024-06-06 14:27:31 -0300

    ok

--- gerenciamento/src/main/java/com/example/gerenciamento/Controller/indexController.java
+++ gerenciamento/src/main/java/com/example/gerenciamento/Controller/indexController.java
@@ -2,6 +2,7 @@ package com.example.gerenciamento.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
